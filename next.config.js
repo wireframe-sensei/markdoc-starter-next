7270f6d80f9667a14619d82b79e81eb631d27c2f
Yaswanth Reddy Ubbara <ubbarayaswanthreddy@gmail.com>
2023-08-25 11:12:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const withMarkdoc = require('@markdoc/next.js');
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 module.exports =
   withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
   });
